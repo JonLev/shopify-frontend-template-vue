38ef45ea13f357233eeb3976f61ba9229cc9bced
JonLev <jonas@together.do>
2023-10-31 11:35:26 +0100

    Fix import

--- src/composables/useAppBridge.ts
+++ src/composables/useAppBridge.ts
@@ -3,7 +3,7 @@ import { createApp } from '@shopify/app-bridge';
 const host = new URLSearchParams(location.search).get('host') || window.__SHOPIFY_DEV_HOST
 window.__SHOPIFY_DEV_HOST = host
 
-export const ueAppBridge = createSharedComposable(() => {
+export const useAppBridge = createSharedComposable(() => {
   const appBridge = createApp({
     apiKey: process.env.SHOPIFY_API_KEY as string,
     host: host as string,
