6d8081dcc81faabf7800fb592e8c8c6cd238d8ab
JonLev <jonas@together.do>
2023-10-31 11:37:26 +0100

    Fix usage

--- src/components/Home/ProductCard.vue
+++ src/components/Home/ProductCard.vue
@@ -13,7 +13,7 @@
       <Text as="h4" variant="headingMd">
         {{ t('ProductsCard.totalProductsHeading') }}
         <Text variant="bodyMd" as="p" fontWeight="semibold">
-          {{ isLoadingCount ? "-" : data.count }}
+          {{ isLoadingCount ? "-" : productCount }}
         </Text>
       </Text>
     </TextContainer>
@@ -27,7 +27,7 @@ import { useProductStore } from 'src/stores/productStore';
 
 const { t } = useTranslation();
 const { appBridge } = useAppBridge();
-const { data, isLoading, setIsLoading, isLoadingCount, addProduct, refetchProductCount, productsCount } = useProductStore();
+const { isLoading, setIsLoading, isLoadingCount, addProduct, refetchProductCount, productsCount } = useProductStore();
 
 const handleAddProduct = async () => {
   const response = await addProduct();
